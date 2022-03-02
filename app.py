de7ce1315ab439b54099c052dcf4088ba22601bb
Lyu <ucmd2001@gmail.com>
2022-03-02 16:28:37 +0800

    make it better

--- app.py
+++ app.py
@@ -221,4 +221,4 @@ def internal_error(error):
     return render_template('error.html',message=message), 500
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0",port=3000,debug=True)
+    app.run()
